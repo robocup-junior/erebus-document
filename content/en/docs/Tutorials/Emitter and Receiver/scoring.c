9486e280811f98ae3099f4b7ffbce8e7f754cda5
victorhu3 <victorhu3@gmail.com>
2021-04-20 01:03:12 -0400

    emitter documentation fix

--- content/en/docs/Tutorials/Emitter and Receiver/scoring.c
+++ content/en/docs/Tutorials/Emitter and Receiver/scoring.c
@@ -1,4 +1,11 @@
+#include <cstring>
+
+...
+
 char message[9]; // Here we use a 9 byte array, since sizeof(int + int + char) = 9
-sprintf(message, "%d%d%c", 0, 0, 'H'); // Format the array with the data X-Pos = 0 cm, Z-Pos = 0 cm, victim type = harmed 
-    
+
+int pos[] = {0 , 0}; // The estimated position of the victim is 0 cm, 0 cm
+memcpy(message, pos, sizeof(pos)); // Copy the position into the message array
+message[8] = 'H'; // The victim type is harmed
+
 emitter->send(message, sizeof(message)); // Send out the message array. Note that the 2nd parameter must be the size of the message
